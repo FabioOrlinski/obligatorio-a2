7aaceca3eddc494cbe5874c30b1a447e7019a5d6
Fabio Orlinski <fabio.orlinski@norhart.com>
2023-06-20 16:39:42 -0300

    ultimo

--- ejercicio7.cpp
+++ ejercicio7.cpp
@@ -14,6 +14,11 @@ public:
   Elemento(int tamano, int lineasCodigo, int puntaje) : tamano(tamano), lineasCodigo(lineasCodigo), puntaje(puntaje) {}
 };
 
+int max(int a, int b)
+{
+    return a > b ? a : b;
+}
+
 void mochilaDP(Elemento **elementos, int cantArchivos, int maximoTamano, int maximoLineasCodigo)
 {
   int ***matDP = new int **[cantArchivos];
@@ -25,9 +30,10 @@ void mochilaDP(Elemento **elementos, int cantArchivos, int maximoTamano, int max
         matDP[i][j][0] = 0;
         }
   }
+
   for (int j = 1; j <= maximoTamano; j++) {
     for (int k = 1; k <= maximoLineasCodigo; k++) {
-      bool condicionTamano= j / elementos[0]->tamano >=1;
+      bool condicionTamano= j/ elementos[0]->tamano >=1;
       bool condicionLineas= k/ elementos[0]->lineasCodigo >=1;
       matDP[0][j][k] = (condicionTamano && condicionLineas) ? elementos[0]->puntaje : 0;
     }
@@ -36,24 +42,30 @@ void mochilaDP(Elemento **elementos, int cantArchivos, int maximoTamano, int max
   for (int i = 1; i < cantArchivos; i++) {
     for (int j = 1; j <= maximoTamano; j++) {
         for (int k = 1; k <= maximoLineasCodigo; k++) {
-          bool condicionTamano= j / elementos[0]->tamano >=1;
-          bool condicionLineas= k/ elementos[0]->lineasCodigo >=1;
+          bool condicionTamano= j < elementos[i]->tamano;
+          bool condicionLineas= k < elementos[i]->lineasCodigo;
           int valorAnt = matDP[i-1][j-1][k];
-//int posibleValor =elementos[i]->puntaje + matDP[i][j-elementos[i]->tamano][k-elementos[i]->lineasCodigo];
-          matDP[i][j][k] = ((j < elementos[i]->tamano) || (k < elementos[i]->lineasCodigo)) ? matDP[i-1][j-1][k] :
-              max(matDP[i-1][j-1][k], elementos[i]->puntaje + matDP[i][j-elementos[i]->tamano][k-elementos[i]->lineasCodigo]);
+          matDP[i][j][k] = (condicionTamano || condicionLineas) ? valorAnt :
+              max(valorAnt, elementos[i]->puntaje + matDP[i][j-elementos[i]->tamano][k-elementos[i]->lineasCodigo]);
         }
     }
   }
   cout << matDP[cantArchivos -1][maximoTamano -1][maximoLineasCodigo - 1] << endl;
+  for (int i = 0; i < cantArchivos; i++) {
+    for (int j = 0; j <= maximoTamano; j++) {
+      delete[] matDP[i][j];
+    }
+    delete[] matDP[i];
+  }
+  delete[] matDP;
 }
 
 int main()
 {
-  //ifstream myFile("Pruebas/Ejercicio7/10.in.txt");
-  //cin.rdbuf(myFile.rdbuf());
-  //ofstream myFile2("Pruebas/Ejercicio7/10.prueba.txt");
-  //cout.rdbuf(myFile2.rdbuf());
+  ifstream myFile("Pruebas/Ejercicio7/100.in.txt");
+  cin.rdbuf(myFile.rdbuf());
+  ofstream myFile2("Pruebas/Ejercicio7/100.prueba.txt");
+  cout.rdbuf(myFile2.rdbuf());
   int cantArchivos, maximoTamano, maximoLineasCodigo;
   cin >> cantArchivos >> maximoTamano >> maximoLineasCodigo;
   Elemento **elementos = new Elemento *[cantArchivos];
